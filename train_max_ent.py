073a406c837d763aee7d75dd633d85c0b099082c
guy <guytenn@gmail.com>
2020-12-09 11:57:53 +0200

    train dqn in sb3

--- train_max_ent.py
+++ train_max_ent.py
@@ -2,23 +2,31 @@ import gym
 import envs
 import numpy as np
 
-from stable_baselines3 import DQN
+from stable_baselines3 import DQN, PPO
 from stable_baselines3.dqn import MlpPolicy
+from tqdm import tqdm
+from stable_baselines3.common.vec_env import DummyVecEnv
 
-env = gym.make('rooms-v0', rows=16, cols=16, horz_wind=(0, 0), vert_wind=(0, 0))
+
+def eval_policy(env, policy, steps=10000, desc=''):
+    obs = env.reset()
+    traj_rewards = [0]
+    for _ in tqdm(range(steps), desc=desc, leave=True):
+        action, _state = policy(obs, deterministic=False)
+        # print(f" time: {_}, action: {action}, obs.shape: {obs.shape}")
+        obs, reward, done, info = env.step(action)
+        traj_rewards[-1] += reward
+        if done:
+            obs = env.reset()
+            traj_rewards.append(0)
+    return np.mean(traj_rewards)
+
+
+env = DummyVecEnv([lambda: gym.make('rooms-v0', rows=16, cols=16, horz_wind=(0, 0), vert_wind=(0, 0))])
 
 model = DQN(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=10000, log_interval=4)
 model.save("rooms")
 
-del model  # remove to demonstrate saving and loading
-
-model = DQN.load("rooms")
-
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
+eval_res = eval_policy(env, model.predict, desc='Evaluating model')
+print(f'Eval Result = {eval_res}')
