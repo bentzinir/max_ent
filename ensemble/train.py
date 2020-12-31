b7f7b29c5fdc51595b21c942c418e8ae761b519f
bentzinir <bentzinir@gmail.com>
2020-12-31 13:58:37 +0200

    1. (ensemble) DQN.yaml: rearranged after introducing "main" config 2. rooms.yaml: new "main" config 3. (ensemble) train.py: modified config access keys due to new rearrangement.

--- ensemble/train.py
+++ ensemble/train.py
@@ -8,6 +8,7 @@ from ensemble.dummy_ensemble_vec_env import DummyEnsembleVecEnv
 from ensemble.discriminator_trainer import DiscriminatorTrainer
 from gym import spaces
 from config.parser_args import get_config
+from config.config import Config
 import argparse
 
 
@@ -29,10 +30,10 @@ def eval_policy(env, model):
             traj_rewards.append(0)
 
 
-def train(env_id, env_kwargs):
-    env = DummyEnsembleVecEnv([lambda: gym.make(env_id, **env_kwargs)], **config.buffer)
+def train(config):
+    env = DummyEnsembleVecEnv([lambda: gym.make(config.env_id, **config.env_kwargs)], **config.algorithm.buffer)
     obs_shape = list(env.observation_space.shape)
-    if config.discrete:
+    if config.algorithm.discrete:
         from ensemble.max_ent_dqn import MaxEntDQN as Algorithm
         from stable_baselines3.dqn.policies import CnnPolicy as DiscriminationModel
         disc_obs_shape = (obs_shape[2], *obs_shape[:2])
@@ -48,46 +49,28 @@ def train(env_id, env_kwargs):
                                     high=env.observation_space.high.max(),
                                     shape=disc_obs_shape,
                                     dtype=env.observation_space.dtype)
-    if config.alg.method == 'state':
-        discrimination_model = DiscriminationModel(observation_space=disc_obs_space,
-                                                   action_space=spaces.Discrete(config.alg.ensemble_size),
-                                                   lr_schedule=lambda x: config.alg.learning_rate).to(config.device)
-        discrimination_trainer = DiscriminatorTrainer(discrimination_model=discrimination_model, discrete=config.discrete)
+    if config.algorithm.policy.method == 'state':
+        d_model = DiscriminationModel(observation_space=disc_obs_space,
+                                      action_space=spaces.Discrete(config.ensemble_size),
+                                      lr_schedule=lambda x: config.algorithm.policy.learning_rate).to(config.device)
+        discrimination_trainer = DiscriminatorTrainer(discrimination_model=d_model, discrete=config.algorithm.discrete)
     else:
         discrimination_trainer = None
 
-    config.alg.device = config.device
-    model = Algorithm(policy, env, discrimination_trainer=discrimination_trainer, **config.alg)
+    model = Algorithm(policy, env, discrimination_trainer=discrimination_trainer, **config.algorithm.policy)
 
-    model.learn(**config.learn)
+    model.learn(**config.algorithm.learn)
     eval_policy(env, model)
-    model.save(env_id)
+    model.save(config.env_id)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--f", type=str, default="none")
     args, extra_args = parser.parse_known_args()
-    config = get_config("DQN")
-    # config = get_config("SAC")
-    config.buffer.ensemble_size = config.alg.ensemble_size
+    config = get_config(args.f)
+    config["algorithm"] = Config(config.algorithm)
+    config.algorithm.buffer["ensemble_size"] = config.ensemble_size
+    config.algorithm.policy["ensemble_size"] = config.ensemble_size
 
-    env_id = 'rooms-v0'
-    room_size = 10
-    rooms_kwargs = {
-        'n_redundancies': 4,
-        'empty': False,
-        'fixed_reset': True,
-        'max_repeats': 1,
-        'rows': room_size,
-        'cols': room_size,
-        'discrete': config.discrete,
-        'goal': [1, 1],
-        'state': [room_size - 2, room_size - 2],
-        'horz_wind': (0.0, 0.0),  # (right, left)
-        'vert_wind': (0.0, 0.0),  # (up, down)
-        'seed': 0
-    }
-
-    train(env_id=env_id, env_kwargs=rooms_kwargs)
-
+    train(config)
