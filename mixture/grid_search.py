eb67ef7a3caed150ee8832f73088a279c8c9cb40
nir-Laptop <bentzinir@gmail.com>
2021-02-01 11:34:23 +0200

    added args

--- mixture/grid_search.py
+++ mixture/grid_search.py
@@ -1,36 +1,42 @@
 import os
 import time
+import argparse
 
-envs = ['Hopper-v2', 'HalfCheetah-v2', 'Ant-v2', 'Swimmer-v2', 'Walker2d-v2', 'Humanoid-v2']
-methods = ['entropy', 'next-action', 'mixture', 'semi-mixture']
-n_repeats = 2
-ensemble_sizes = [3]
-step_mixture = 'true'
-total_timesteps = 3000000
-buffer_size = 300000
-log_interval = 1  # (episodes)
-wandb_log_interval = 10000  # (timesteps)
 
-# Debug
-n_repeats = 2
-buffer_size = 1000000
-envs = ['Hopper-v2']
-methods = ['mixture', 'semi-mixture', 'entropy']
-# ensemble_sizes = [1, 2, 3]
+def run(envs, ensemble_sizes, methods, n_repeats, wandb_log_interval, total_timesteps, pause):
 
-for trials in range(n_repeats):
-    for env in envs:
-        for method in methods:
-            for ens_size in ensemble_sizes:
-                cmd_line = f"python -m mixture.train --f=mixture/config/mujoco " \
-                           f" --wandb_log_interval {wandb_log_interval} " \
-                           f" --total_timesteps {total_timesteps} " \
-                           f" --algorithm.learn.log_interval {log_interval} " \
-                           f" --step_mixture {step_mixture}" \
-                           f" --ensemble_size {ens_size} " \
-                           f" --algorithm.policy.buffer_size {buffer_size} " \
-                           f" --method {method} " \
-                           f" --env_id {env} & "
-                print(cmd_line)
-                os.system(cmd_line)
-                time.sleep(0.1)
+    step_mixture = 'true'
+    buffer_size = 500000
+    log_interval = 1  # (episodes)
+
+    for trials in range(n_repeats):
+        for env in envs:
+            for method in methods:
+                for ens_size in ensemble_sizes:
+                    cmd_line = f"python -m mixture.train --f=mixture/config/mujoco " \
+                               f" --wrapper_kwargs.wandb_log_interval {wandb_log_interval} " \
+                               f" --total_timesteps {total_timesteps} " \
+                               f" --algorithm.learn.log_interval {log_interval} " \
+                               f" --step_mixture {step_mixture}" \
+                               f" --ensemble_size {ens_size} " \
+                               f" --algorithm.policy.buffer_size {buffer_size} " \
+                               f" --method {method} " \
+                               f" --env_id {env} & "
+                    print(cmd_line)
+                    os.system(cmd_line)
+                    time.sleep(pause)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--envs", nargs="+",
+                        default=['Hopper-v2', 'HalfCheetah-v2', 'Ant-v2', 'Swimmer-v2', 'Walker2d-v2', 'Humanoid-v2'])
+    parser.add_argument("--ensemble_sizes", nargs="+", default=[3])
+    parser.add_argument("--methods", nargs="+", default=['mixture', 'semi-mixture', 'entropy'])
+    parser.add_argument("--n_repeats", type=int, default=2)
+    parser.add_argument("--wandb_log_interval", type=int, default=10000)
+    parser.add_argument("--total_timesteps", type=int, default=2000000)
+    parser.add_argument("--pause", type=float, default=0.1)
+
+    args, extra_args = parser.parse_known_args()
+    run(**args.__dict__)
