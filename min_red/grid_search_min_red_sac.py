74e556444d4a192b253159f91f367d5f292a9b1d
nir-Laptop <bentzinir@gmail.com>
2021-02-02 18:42:13 +0200

    1. grid_search_min_red_sac.py: New file for min_red sac grid search

---
+++ min_red/grid_search_min_red_sac.py
@@ -0,0 +1,39 @@
+import os
+import time
+import argparse
+
+
+def run(envs, ent_coef, methods, n_repeats, wandb_log_interval, total_timesteps, pause, dry):
+
+    log_interval = 1
+
+    for trials in range(n_repeats):
+        for env in envs:
+            for method in methods:
+                cmd_line = f"python -m min_red.train --f min_red/config/mujoco --algorithm_type SAC " \
+                           f" --wrapper_kwargs.wandb_log_interval {wandb_log_interval} " \
+                           f" --method {method} " \
+                           f" --algorithm.policy.ent_coef {ent_coef} " \
+                           f" --total_timesteps {total_timesteps} " \
+                           f" --algorithm.learn.log_interval {log_interval} " \
+                           f" --env_id {env} & "
+                print(cmd_line)
+                if not dry:
+                    os.system(cmd_line)
+                    time.sleep(pause)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--envs", nargs="+",
+                        default=['Hopper-v2', 'HalfCheetah-v2', 'Ant-v2', 'Swimmer-v2', 'Walker2d-v2', 'Humanoid-v2'])
+    parser.add_argument("--methods", nargs="+", default=['action', 'stochastic'])
+    parser.add_argument("--n_repeats", type=int, default=2)
+    parser.add_argument("--wandb_log_interval", type=int, default=10000)
+    parser.add_argument("--ent_coef", type=float, default=0.1)
+    parser.add_argument("--total_timesteps", type=int, default=3000000)
+    parser.add_argument("--pause", type=float, default=0.1)
+    parser.add_argument("--dry", action='store_true')
+
+    args, extra_args = parser.parse_known_args()
+    run(**args.__dict__)
